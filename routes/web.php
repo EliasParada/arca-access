a8ed52cfae0930eaf16bf0a943cdd388641c2eee
EliasParada <eliasparadalozano@gmail.com>
2024-05-09 19:29:40 -0600

    Home controller

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\homeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('login', [App\Http\Controllers\LoginController::class, 'index'])->name('login');
 Route::get('register', [App\Http\Controllers\RegisterController::class, 'index'])->name('register');
